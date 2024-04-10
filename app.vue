f5c138093346d0487680ff2190e6e22d169534c6
GiveMeFox <givemefoxlang@gmail.com>
2024-04-10 11:01:32 +0200

    Chipi chipi chapa chapa Dubi dubi daba daba Magical my dubi dubi Boom, boom, boom, boom , boom Chipi chipi chapa chapa Dubi dubi daba daba Magical my dubi dubi Boom Chipi chipi chapa chapa Dubi dubi daba daba Magical my dubi dubi Boom, boom, boom, boom, boom Chipi chipi chapa chapa Dubi dubi daba daba Magical my dubi dubi Boom

--- app.vue
+++ app.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
-
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'Login',
+  icon: 'i-heroicons-user-circle',
+  to: 'login'
+}]
 </script>
-
 
 <template>
   <div class="h-screen">
+    <nav>
+      <UBreadcrumb :links="links" />
+    </nav>
+
     <NuxtLayout >
       <NuxtPage/>
     </NuxtLayout>
