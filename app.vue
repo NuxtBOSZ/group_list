e8089b6d26bd6be8e3e6bda8a8c1f40ac34a7c7e
bogusz <bogusz2005@gmail.com>
2024-04-10 14:02:25 +0200

    login.vue

--- app.vue
+++ app.vue
@@ -8,12 +8,41 @@ const links = [{
   icon: 'i-heroicons-user-circle',
   to: 'login'
 }]
+
+const colorMode = useColorMode()
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+
 </script>
 
 <template>
   <div class="h-screen">
-    <nav>
-      <UBreadcrumb :links="links" />
+    <nav class="flex justify-between p-4">
+      <UBreadcrumb :links="links" :ui="{ ol: 'gap-x-3', li: 'gap-x-3' }">
+        <template #divider>
+          <span class="w-8 h-1 rounded-full bg-gray-300 dark:bg-gray-700" />
+        </template>
+      </UBreadcrumb>
+
+      <ClientOnly>
+        <UButton
+            :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+            color="gray"
+            variant="ghost"
+            aria-label="Theme"
+            @click="isDark = !isDark"
+        />
+        <template #fallback>
+          <div class="w-8 h-8"/>
+        </template>
+      </ClientOnly>
+
     </nav>
 
     <NuxtLayout >
